d96665d075682a44d96064bd3c4ef240430df6f1
eskimo1228 <eskimo1228@outlook.com>
2020-04-10 19:18:54 +0300

    Fix and updated

--- messages.php
+++ messages.php
@@ -2,7 +2,6 @@
   <div class="msg alert-danger">
   	<?php foreach ($errors as $error) : ?>
   	  <span><?php echo $error ?></span>
-	  <span></span>
   	<?php endforeach ?>
   </div><br>
 <?php  endif ?>
